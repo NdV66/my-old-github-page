55c19e7f6b4c350a811144ac141e536a76e648da
Marta Zażlak <marta.zazlak@hotmail.com>
2016-09-24 23:50:06 +0200

    full src

--- js/main.js
+++ js/main.js
@@ -1,1 +1,150 @@
-$(document).ready(function(){var n=n||{};n.overlay=function(n){function t(n){s=n,console.log("RESIZED! "+s)}function e(n){o.scroll(function(){var t=$(n).outerHeight(),e=o.scrollTop(),a=0;t-e>0&&(a=s-e,a+="px",i.css({top:a}))})}var o=$(window),i=$(n),s=i.offset().top;return{startOverlaing:e,setPositionTop:t}}("#overlay"),n.imageHeight=function(){function n(){e.css({height:o.outerHeight()+"px"})}function t(){return e.outerHeight()}var e=$("#image"),o=$(window);return{set:n,get:t}}(),n.switchNames=function(){function n(){$("#names").on("click",function(){function n(){t.hasClass("hidden")?(t.removeClass("hidden"),e.addClass("hidden")):(e.removeClass("hidden"),t.addClass("hidden"))}$("#names>span:not(.hidden)").animate({top:"400px",opacity:"0"},900,function(){$("#names>span:not(.hidden)").css({top:"0",opacity:"1"}),n()})})}var t=$("#nick"),e=$("#real-name");return{bindSwitchNames:n}}(),n.scrollToElement=function(){function n(n){var t=$(window);$("html,body").animate({scrollTop:$(n).offset().top-t.outerHeight()},1e3),console.log(t.outerHeight())}function t(){$(".buttons button").each(function(){$(this).on("click",function(){var t=$(this).attr("id"),e=t.replace(/-button/g,"");n("#"+e)})})}return{bindAll:t}}();var t=n.imageHeight,e=n.overlay;t.set(),n.switchNames.bindSwitchNames(),e.startOverlaing("#image"),n.scrollToElement.bindAll(),$(window).resize(function(){t.set(),e.setPositionTop(t.get()),$(this).trigger("scroll")})});
+$(document).ready(function main(){
+/*------ ------ ------ ------ TOOLS ------ ------ ------ ------*/
+    var MAIN = MAIN || {}; 
+    
+    /*------ ------ ------ ------ create overlaing ------ ------ ------ ------*/
+    MAIN.overlay = (function init(overlayId){        
+        var $window = $(window),
+            overlay = $(overlayId),
+            sectionPositionTop = overlay.offset().top; //on start
+
+        function setPositionTop(newPosition){
+            sectionPositionTop = newPosition;
+            console.log("RESIZED! " + sectionPositionTop);
+        }
+        /**
+         * This function creates an overlaing of div#image. 
+         * @param {String} ID of image - this image with names
+         * @returns {undefined}
+         */      
+        function startOverlaing(imageId) {                        
+            $window.scroll(function onScroll(){                
+                var imageHeight = $(imageId).outerHeight(),
+                    scrollTop = $window.scrollTop(),
+                    offsetValue = 0;
+
+                if(imageHeight - scrollTop > 0) {
+                    offsetValue = sectionPositionTop - scrollTop;
+                    offsetValue = offsetValue + 'px';
+                    overlay.css({
+                        'top': offsetValue
+                    });
+                }
+            });
+        }
+
+        return {
+            startOverlaing: startOverlaing,
+            setPositionTop: setPositionTop
+        };  
+    })('#overlay');   
+         
+    
+    /*------ ------ ------ ------ set image's height ------ ------ ------ ------*/
+    MAIN.imageHeight = (function imageHeight(){
+        var image = $('#image'),
+            $window = $(window);        
+        
+        function set(){
+            image.css({
+                'height': $window.outerHeight() + 'px'
+            });                   
+        }
+        
+        function get() {
+            return image.outerHeight();
+        }
+        
+        return {
+            set: set,
+            get: get
+        }; 
+    })();
+    
+    /*------ ------ ------ ------ switch names ------ ------ ------ ------*/
+    MAIN.switchNames = (function(){
+        var nick = $('#nick'),
+            name = $('#real-name');
+        
+        /**
+         * Binf event to switch names
+         * @returns {undefined}
+         */
+        function bindSwitch() {
+            $('#names').on('click', function switchNames(){
+                
+                function setHidden() {
+                    if(nick.hasClass('hidden')) {
+                        nick.removeClass('hidden');
+                        name.addClass('hidden');
+                    } else {
+                        name.removeClass('hidden');
+                        nick.addClass('hidden');
+                    }   
+                }
+                
+                $('#names>span:not(.hidden)').animate({
+                    'top': '400px',
+                    'opacity': '0'
+                }, 900, function afterAimate(){
+                    $('#names>span:not(.hidden)').css({
+                        'top': '0',
+                        'opacity': '1'
+                    });        
+                    setHidden();
+                });   
+            });  
+        }
+        
+        return {
+            bindSwitchNames: bindSwitch
+        };
+    })();
+    
+    /*------ ------ ------ ------ buttons on image controller ------ ------ ------ ------*/
+    MAIN.scrollToElement = (function init(){
+        function scrollToElement(elementId) {
+            var $window = $(window);
+            $('html,body').animate({
+                scrollTop: $(elementId).offset().top - $window.outerHeight()}, //offset - image heigt (this image with names) + correction
+                1000);
+                console.log($window.outerHeight());
+        }
+        
+        /*
+         * Binds all function-buttons to a event
+         * @returns {undefined}
+         */
+        function bindAll() {
+            $('.buttons button').each(function(){
+                $(this).on('click', function() {
+                    var id = $(this).attr('id'),
+                        div = id.replace(/-button/g, '');
+                    scrollToElement('#' + div);
+                });
+            });
+        }
+        
+        return {
+            bindAll: bindAll
+        };
+    })();       
+    
+    /*------ ------ ------ ------ Simple names of module ------ ------ ------ ------*/
+   var imageHeight = MAIN.imageHeight,
+        overlay = MAIN.overlay;
+    
+
+/*------ ------ ------ ------ LOGICAL PART ------ ------ ------ ------*/  
+    imageHeight.set();   
+    MAIN.switchNames.bindSwitchNames();    
+    overlay.startOverlaing('#image');
+    MAIN.scrollToElement.bindAll();
+    
+    $(window).resize(function(){
+        imageHeight.set();
+        overlay.setPositionTop(imageHeight.get());
+        $(this).trigger('scroll');
+    });    
+});
+
